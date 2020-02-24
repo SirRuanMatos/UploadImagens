27252b86930cec9fc16bc8dbe07c4809a29f6f9b
Ruan <des.ruan@gmail.com>
2020-02-24 14:14:00 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors")
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:32768/upload",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use('/files', express.static(path.resolve(__dirname,'tmp','uploads')))
+
+
+app.use(require("./routes"));
+
+app.listen(3000);
